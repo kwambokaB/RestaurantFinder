8a839ced01da3b56c71d447f9a721264e8eef0d6
kwambokaB <paula.paulatek@gmail.com>
2020-11-02 20:07:21 +0300

    implementig the major routes and connecting then with the db

--- Server/server.js
+++ Server/server.js
@@ -26,16 +26,92 @@ app.get('/api/v1/restaurants', async (req, res) => {
     catch (e) {
         console.log(e)
         res.status(500).json({
-            status: 'failed',
+            status: 'request failed',
             Error: e
         })
     }
    
 });
-app.get('/api/v1/restaurant/:id', (req, res) => res.send('get one restaurant'));
-app.post('/api/v1/restaurant', (req, res) => res.send('add restaurant'));
-app.put('api/v1/restaurant/:id', (req, res) => res.send('update restaurant'));
-app.delete('api/v1/restaurant/:id', (req, res) => res.send('delete restaurant'));
+
+app.get('/api/v1/restaurant/:id', async (req, res) => {
+try {
+   const restaurant =  await db.query('select * from restaurants where id = $1', [req.params.id]);
+   res.status(200).json({
+       status: 'success',
+       data: restaurant.rows[0]
+   })
+
+}
+catch (e) {
+    console.log(e)
+        res.status(500).json({
+            status: ' request failed',
+            Error: e
+        })
+}
+});
+
+
+app.post('/api/v1/restaurant', async(req, res) => {
+ try{
+     const newRestaurant = await db.query("INSERT INTO restaurants (name, location, price_range) values ($1, $2, $3) returning *", [req.body.name, req.body.location, req.body.price_range] )
+     res.status(200).json({
+        status: 'success',
+        data: newRestaurant.rows[0]
+    })
+ }
+ catch(e) {
+    console.log(e)
+    res.status(500).json({
+        status: ' request failed',
+        Error: e
+    })
+ }
+
+});
+
+
+app.put('/api/v1/restaurant/:id', async(req, res) => { 
+
+    try{
+        const updatedRestaurant = await db.query("UPDATE restaurants SET name = $1, location = $2, price_range = $3 where id = $4 returning *",
+         [req.body.name, req.body.location, req.body.price_range, req.params.id])
+        res.status(200).json({
+            status: 'success',
+            data: updatedRestaurant.rows[0]
+        })
+    } 
+
+    catch(e) {
+        console.log(e)
+        res.status(500).json({
+            status: ' request failed',
+            Error: e
+        })
+    }
+
+});
+
+
+app.delete('/api/v1/restaurant/:id', async (req, res) => {
+   try {
+       const deleteRestaurant = await db.query('DELETE FROM restaurants where id = $1', [req.params.id]);
+
+       res.status(200).json({
+        status: 'success',
+        data: deleteRestaurant
+    })
+
+   }
+   catch (e) {
+
+    console.log(e)
+    res.status(500).json({
+        status: ' request failed',
+        Error: e
+    })
+   }
+});
 
 
 
